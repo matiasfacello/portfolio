7ea0e5342cac52c9de125035ca2fb8e9f2f070f3
matiasfacello <matiasfacello@gmail.com>
2022-09-18 20:03:45 -0300

    navbar with background and one less section

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,14 +1,20 @@
-function Navbar() {
+import "./navbar.css";
 
+function Navbar() {
   return (
-  <nav className="container fixed top-0 h-20 m-auto">
-    <div className="flex flex-wrap items-center justify-center h-full gap-4 text-2xl md:gap-12 text-stone-300">
-      <a href="#home" className="link hover:text-stone-100 md:p-2 active:text-stone-100">home</a>
-      <a href="#about" className="link hover:text-stone-100 md:p-2 active:text-stone-100">about me</a>
-      <a href="#portfolio" className="link hover:text-stone-100 md:p-2 active:text-stone-100">portfolio</a>
-      <a href="#contact" className="link hover:text-stone-100 md:p-2 active:text-stone-100">contact</a>
-    </div>
-  </nav>
+    <nav className="fixed top-0 w-full h-12">
+      <div className="container flex flex-wrap items-center justify-end h-full gap-2 m-auto text-lg md:gap-6 text-stone-300">
+        <a href="#home" className="px-2 link hover:text-stone-100 active:text-stone-100">
+          home
+        </a>
+        <a href="#about" className="px-2 link hover:text-stone-100 active:text-stone-100">
+          about me
+        </a>
+        <a href="#portfolio" className="px-2 link hover:text-stone-100 active:text-stone-100">
+          portfolio
+        </a>
+      </div>
+    </nav>
   );
 }
 

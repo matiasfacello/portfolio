ec42ca16fa2fa7bd8adf26c677703e0bc35e288c
matiasfacello <matiasfacello@gmail.com>
2022-09-18 17:48:43 -0300

    format: adds prettier config and some format
    next files probably are changing format too

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -5,14 +5,11 @@ interface IconInterface {
   src: string;
 }
 
-const Icon = ({src,name}:IconInterface) => {
-
+const Icon = ({ src, name }: IconInterface) => {
   let imgSrc = icon_list[src as keyof typeof icon_list];
   let imgId = src + "Icon";
-  
-  return (
-    <img src={imgSrc} alt={name} id={imgId} width="60" height="66" className="p-2"/>
-  );
-}
+
+  return <img src={imgSrc} alt={name} id={imgId} width="60" height="66" className="p-2" />;
+};
 
 export default Icon;

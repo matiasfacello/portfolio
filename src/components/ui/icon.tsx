fb89b08215030f5cfcdec5cfa9e982b34b282f49
matiasfacello <matiasfacello@gmail.com>
2022-09-18 14:06:41 -0300

    feat: icon component and icon list

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,18 @@
+import icon_list from "./icon_list";
+
+interface IconInterface {
+  name: string;
+  src: string;
+}
+
+const Icon = ({src,name}:IconInterface) => {
+
+  let imgSrc = icon_list[src as keyof typeof icon_list];
+  let imgId = src + "Icon";
+  
+  return (
+    <img src={imgSrc} alt={name} id={imgId} width="60" height="66" className="p-2"/>
+  );
+}
+
+export default Icon;
---
+++ src/components/ui/icon_list.tsx
@@ -0,0 +1,37 @@
+import bootstrap from '../../assets/icons/bootstrap.svg';
+import css from '../../assets/icons/css.svg';
+import docker from '../../assets/icons/docker.svg';
+import figma from '../../assets/icons/figma.svg';
+import git from '../../assets/icons/git.svg';
+import html from '../../assets/icons/html.svg';
+import javascript from '../../assets/icons/javascript.svg';
+import laravel from '../../assets/icons/laravel.svg';
+import lua from '../../assets/icons/lua.svg';
+import nginx from '../../assets/icons/nginx.svg';
+import nodejs from '../../assets/icons/nodejs.svg';
+import photoshop from '../../assets/icons/photoshop.svg';
+import php from '../../assets/icons/php.svg';
+import react from '../../assets/icons/react.svg';
+import svelte from '../../assets/icons/svelte.svg';
+import tailwind from '../../assets/icons/tailwind.svg';
+import wordpress from '../../assets/icons/wordpress.svg';
+
+export default {
+    'bootstrap':bootstrap,
+    'css':css,
+    'docker':docker,
+    'figma':figma,
+    'git':git,
+    'html':html,
+    'javascript':javascript,
+    'laravel':laravel,
+    'lua':lua,
+    'nginx':nginx,
+    'nodejs':nodejs,
+    'photoshop':photoshop,
+    'php':php,
+    'react':react,
+    'svelte' :svelte,
+    'tailwind':tailwind,
+    'wordpress':wordpress
+};

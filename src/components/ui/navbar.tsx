eb55d882277db6d4f8d7e6bb3cd07c1af43a0cf1
matiasfacello <matiasfacello@gmail.com>
2022-09-21 22:12:38 -0300

    style: updates navbar links hover to light blue

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,13 +4,13 @@ function Navbar() {
   return (
     <nav className="fixed top-0 w-full h-12">
       <div className="container flex flex-wrap items-center justify-end h-full gap-2 m-auto text-lg md:gap-6 text-stone-300">
-        <a href="#home" className="px-2 link hover:text-stone-100 active:text-stone-100">
+        <a href="#home" className="px-2 link hover:text-blue-300 active:text-blue-300">
           home
         </a>
-        <a href="#about" className="px-2 link hover:text-stone-100 active:text-stone-100">
+        <a href="#about" className="px-2 link hover:text-blue-300 active:text-blue-300">
           about me
         </a>
-        <a href="#portfolio" className="px-2 link hover:text-stone-100 active:text-stone-100">
+        <a href="#portfolio" className="px-2 link hover:text-blue-300 active:text-blue-300">
           portfolio
         </a>
       </div>

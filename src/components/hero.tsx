9274ce47c66e0c36b1d93e33dc6b816b50d1b1f9
matiasfacello <matiasfacello@gmail.com>
2022-09-23 18:14:14 -0300

    updated name in hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,9 +5,8 @@ function Hero() {
     <section id="home" className="container min-h-screen m-auto">
       <div className="flex flex-row justify-start">
         <div className="flex flex-col items-start justify-center min-h-screen p-16">
-          <h1 className="my-1 text-4xl md:text-8xl">matias facello</h1>
-          <h2 className="text-2xl md:text-4xl">fullstack web developer</h2>
-          <h2 className="mb-16 text-2xl md:text-4xl">front-end web developer</h2>
+          <h1 className="my-1 text-4xl md:text-8xl">Matias Facello</h1>
+          <h2 className="mb-16 text-2xl md:text-4xl">Web Desginer / Web Developer</h2>
           <Contact />
         </div>
       </div>

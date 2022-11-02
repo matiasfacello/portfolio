44ad39db592b802ab3c845c4f90f03468a64a0cb
matiasfacello <matiasfacello@gmail.com>
2022-11-01 23:13:21 -0300

    adds bottom text naming the icons

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -9,7 +9,12 @@ const Icon: React.FC<IconInterface> = ({ src, name }) => {
   let imgSrc = icon_list[src];
   let imgId = src + "Icon";
 
-  return <img src={imgSrc} alt={name} id={imgId} width="60" height="66" className="p-2" />;
+  return (
+    <div className="flex flex-col items-center justify-end px-2 mb-2 transition hover:scale-125">
+      <img src={imgSrc} alt={name} id={imgId} width="60" height="66" className="p-1 mb-1" />
+      <p className="text-sm font-bold">{name}</p>
+    </div>
+  );
 };
 
 export default Icon;

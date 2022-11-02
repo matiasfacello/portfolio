b185a64c017bc78f72bc504ecf0b1ace8e719de0
matiasfacello <matiasfacello@gmail.com>
2022-11-01 23:11:17 -0300

    adds and renews old portfolio images

--- src/components/ui/project_images.tsx
+++ src/components/ui/project_images.tsx
@@ -1,7 +1,11 @@
-import fmzurich from "../../assets/projects/fmzurich.jpg";
 import cartagena from "../../assets/projects/cartagena.jpg";
+import fmzurich from "../../assets/projects/fmzurich.jpg";
+import portfolio from "../../assets/projects/portfolio.jpg";
+import pristineshock from "../../assets/projects/pristineshock.jpg";
 
 export default {
-  fmzurich: fmzurich,
   cartagena: cartagena,
+  fmzurich: fmzurich,
+  portfolio: portfolio,
+  pristineshock: pristineshock,
 };
